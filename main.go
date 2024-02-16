9830a8292a7c2fc60f1ff41dd70e42aa6a43d04a
Sándor Lipcsei <sandor.lipcsei@gmail.com>
2024-02-16 12:47:09 +0100

    Improve doc for main

--- main.go
+++ main.go
@@ -7,30 +7,40 @@ import (
 	"sync"
 )
 
+// numTasks defines the total number of tasks to be generated and processed.
 const numTasks = 100
 
 func main() {
 
+	// Create channels for tasks and results with a capacity of numTasks.
 	tasks := make(chan worker.Task, numTasks)
 	results := make(chan worker.Result, numTasks)
 
+	// Start a goroutine to generate tasks
 	go worker.GenerateTasks(numTasks, tasks)
 
+	// wg is a WaitGroup to wait for all worker goroutines to finish processing.
 	var wg sync.WaitGroup
+
+	// numWorkers is a determined the number of workers based on the number of CPU cores + 1.
 	numWorkers := runtime.NumCPU() + 1
-	for i := 0; i < numWorkers; i++ {
+	for workerID := 0; workerID < numWorkers; workerID++ {
+		// Increment the WaitGroup counter for each worker.
 		wg.Add(1)
-		w := worker.New(i, tasks, results, &wg)
+		// Initialize a new worker.
+		w := worker.New(workerID, tasks, results, &wg)
+		// Start the worker in a new goroutine.
 		go w.Start()
 	}
 
+	// Start a goroutine to close the results channel once all workers have finished.
 	go func() {
 		wg.Wait()
 		close(results)
 	}()
 
+	// Collect and print the results. SortResults organizes results into their original order.
 	for _, result := range worker.SortResults(results, numTasks) {
 		fmt.Printf("%d. task: %d! = %d\n", result.Task.ID, result.Task.Value, result.Factorial)
 	}
-
 }
